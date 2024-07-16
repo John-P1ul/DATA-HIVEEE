3341998d3e72a974b3b5247a8fba7cb448ff3d81
unknown <sawdickagboke@gmail.com>
2024-07-16 21:05:54 +0100

    Archive

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import List, Optional
-
+from database import create_db_and_tables
 
 class User(SQLModel, table=True):
     id : Optional[int] = Field(default=None, primary_key=True)
@@ -15,3 +15,6 @@ class Upload(SQLModel, table=True):
     filename: str
     data: str
     owner: Optional[User] = Relationship(back_populates="uploads")
+    
+    
+create_db_and_tables()
